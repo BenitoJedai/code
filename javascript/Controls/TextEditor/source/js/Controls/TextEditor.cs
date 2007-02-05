110486001115b84abe0f847d7c7a055b6232dea1
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2007-02-05 07:24:21 +0000

    vversion 4, lorem ipsum as default text

--- javascript/Controls/TextEditor/source/js/Controls/TextEditor.cs
+++ javascript/Controls/TextEditor/source/js/Controls/TextEditor.cs
@@ -439,11 +439,11 @@ namespace MyEditor.source.js.Controls
 
             var d = this.Document;
 
-            d.write("<html><body style='height: auto; border: 0; overflow: auto; background-color:transparent;'></body></html>");
+            
+            d.write("<html><body style='height: auto; border: 0; overflow: auto; background-color:transparent;'>");
+            d.write("<p><span style='font-family: verdana;'><b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span><p>");
+            d.write("</body></html>");
             d.close();
-
-
-            this.InnerHTML = "<p><span style='font-family: verdana;'><b>Lorem</b> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span><p>";
 
 
             //ttoolbar.appendChild(Spinner);
