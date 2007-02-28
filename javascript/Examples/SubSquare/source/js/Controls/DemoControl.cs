05c881f18376f17f42fc4dea94a748eb8e423e66
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2007-02-28 17:42:31 +0000

    SubSquare Example

---
+++ javascript/Examples/SubSquare/source/js/Controls/DemoControl.cs
@@ -0,0 +1,83 @@
+using ScriptCoreLib;
+
+using ScriptCoreLib.JavaScript.Controls;
+using ScriptCoreLib.JavaScript;
+using ScriptCoreLib.JavaScript.Runtime;
+using ScriptCoreLib.JavaScript.Serialized;
+using ScriptCoreLib.JavaScript.DOM.HTML;
+//using ScriptCoreLib.JavaScript.DOM.XML;
+using ScriptCoreLib.JavaScript.DOM;
+
+using ScriptCoreLib.Shared;
+using ScriptCoreLib.Shared.Drawing;
+
+namespace SubSquare.source.js.Controls
+{
+    [Script]
+    public class DemoControl : SpawnControlBase
+    {
+        public const string Alias = "fx.DemoControl";
+
+        IHTMLDiv Control = new IHTMLDiv();
+
+        public IStyle Style { get { return Control.style; } }
+
+
+        void CreateDisposableButton(string e, EventHandler a)
+        {
+
+
+            var btn = new IHTMLButton(e);
+
+            this.Control.appendChild(btn);
+
+            btn.onclick +=
+                delegate
+                {
+                    a();
+
+                    Control.Dispose();
+                };
+        }
+
+        public DemoControl(IHTMLElement e)
+            : base(e)
+        {
+            e.insertNextSibling(Control);
+
+            Native.Document.body.style.background = "#6591cd url(fx/gfx/editorBg.gif) repeat-x";
+            Native.Document.body.style.overflow = IStyle.OverflowEnum.hidden;
+
+            Control.appendChild(new IHTMLElement(IHTMLElement.HTMLElementEnum.h1, 
+                "This project demostrates interactive division of squares into subsquares."));
+
+            Control.appendChild("Hold your mouse over a section. It will take some time to be divided.");
+
+            CreateDisposableButton("Show SubSquares",
+                delegate {
+
+                    var r = Rectangle.Of(
+                            32,
+                            32,
+                            800,
+                            600
+                        );
+
+                    Console.WriteLine("bounds: " + r);
+
+                    new SubSquare(
+                        r
+                    );
+
+                }
+            );
+
+
+        }
+
+
+
+    }
+
+
+}
---
+++ javascript/Examples/SubSquare/source/js/SubSquare.cs
@@ -0,0 +1,111 @@
+using ScriptCoreLib;
+
+using ScriptCoreLib.Shared.Drawing;
+using ScriptCoreLib.Shared;
+
+using ScriptCoreLib.JavaScript;
+using ScriptCoreLib.JavaScript.Runtime;
+using ScriptCoreLib.JavaScript.System;
+using ScriptCoreLib.JavaScript.DOM;
+using ScriptCoreLib.JavaScript.DOM.HTML;
+
+namespace SubSquare.source.js
+{
+
+    [Script]
+    class SubSquare
+    {
+
+        IHTMLDiv Base = new IHTMLDiv();
+
+        Timer timer = new Timer();
+
+        int value = 0xFF;
+
+        Rectangle Location;
+
+        public SubSquare(Rectangle size)
+        {
+            Location = size;
+
+            Base.style.SetLocation(size);
+            Base.style.backgroundColor = Color.White;
+
+            timer.Tick += new EventHandler<Timer>(timer_Tick);
+
+            Base.onmouseover += delegate { if (value > step) timer.StartInterval(40); };
+
+            Base.onmouseout += delegate { timer.Stop(); Base.style.backgroundColor = Color.FromGray(value); };
+
+            Base.attachToDocument();
+        }
+
+        const int step = 5;
+
+        void timer_Tick(Timer e)
+        {
+            if (value > step)
+            {
+                Console.WriteLine("tick");
+
+                value -= step;
+
+                Base.style.backgroundColor = Color.Red;
+
+                Base.style.Opacity = value / 0xFF;
+
+                if (value <= step)
+                {
+                    Console.WriteLine("// we are done");
+
+
+                    
+                    timer.Stop();
+
+                    this.Base.style.backgroundColor = Color.Red;
+
+                    WorkPool p = Fader.FlashAndFadeOut(this.Base, 200);
+
+
+                    p += delegate
+                    {
+                        new SubSquare(
+                        Rectangle.Of(Location.Left, Location.Top, Location.Width / 2, Location.Height / 2)
+                          );
+
+                        new SubSquare(
+                            Rectangle.Of(Location.Left + Location.Width / 2, Location.Top, Location.Width / 2, Location.Height / 2)
+                        );
+
+                        new SubSquare(
+                            Rectangle.Of(Location.Left + Location.Width / 2, Location.Top + Location.Height / 2, Location.Width / 2, Location.Height / 2)
+                        );
+
+                        new SubSquare(
+                            Rectangle.Of(Location.Left, Location.Top + Location.Height / 2, Location.Width / 2, Location.Height / 2)
+                        );
+
+                        this.Base.Dispose();
+                    };
+                }
+            }
+            else
+                timer.Stop();
+        }
+
+        static SubSquare()
+        {
+
+            Native.Spawn("Web.ss",
+                delegate(IHTMLElement e)
+                {
+                    new SubSquare(Rectangle.Of(8,8, Native.Document.body.clientWidth - 16, Native.Document.body.clientHeight - 16));
+
+
+                    e.Dispose();
+                }
+            );
+
+        }
+    }
+}
