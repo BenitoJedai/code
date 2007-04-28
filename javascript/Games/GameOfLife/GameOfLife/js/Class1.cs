8819c06905fa924f48fe7e062ff7eb8b2e98e391
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2007-04-28 14:51:39 +0000

    working life

--- javascript/Games/GameOfLife/GameOfLife/js/Class1.cs
+++ javascript/Games/GameOfLife/GameOfLife/js/Class1.cs
@@ -19,7 +19,54 @@ using ScriptCoreLib.JavaScript.DOM.XML;
 
 namespace GameOfLife.js
 {
+    [Script]
+    class Array2D<T>
+    {
+        readonly T[] items;
 
+        readonly int x;
+        readonly int y;
+
+        public Array2D(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+            this.items = new T[x * y];
+        }
+
+        public void ForEach(Action<int, int> a)
+        {
+            for (int i = 0; i < this.x; i++)
+                for (int j = 0; j < this.y; j++)
+                    a(i, j);
+
+        }
+
+
+
+        public T this[int x, int y]
+        {
+            get
+            {
+                if (x < 0) return default(T);
+                if (y < 0) return default(T);
+                if (x >= this.x) return default(T);
+                if (y >= this.y) return default(T);
+
+                return this.items[this.x * y + x];
+            }
+            set
+            {
+                if (x < 0) return;
+                if (y < 0) return;
+                if (x >= this.x) return;
+                if (y >= this.y) return;
+
+                this.items[this.x * y + x] = value;
+            }
+        }
+    }
 
     [Script]
     public class Class1
@@ -35,13 +82,8 @@ namespace GameOfLife.js
         public Class1(IHTMLElement DataElement)
         {
 
-            IHTMLDiv Control = new IHTMLDiv();
 
-
-            DataElement.insertNextSibling(
-                Control
-
-            );
+            Native.Document.body.style.backgroundColor = Color.System.ThreeDFace;
 
 
             var vv = new ArenaControl();
@@ -50,8 +92,8 @@ namespace GameOfLife.js
 
 
 
-            var cx = 32;
-            var cy = 16;
+            var cx = 24;
+            var cy = 24;
 
             var w = 24;
             var h = 24;
@@ -59,15 +101,13 @@ namespace GameOfLife.js
             vv.SetCanvasSize(new Point(cx * w, cy * h));
 
             vv.SetLocation(new Rectangle { Left = 32, Top = 32, Width = 400, Height = 300 });
+            vv.Layers.Canvas.style.backgroundColor = Color.White;
 
             vv.Control.attachToDocument();
 
-            vv.Layers.Canvas.style.backgroundColor = Color.White;
 
+            var buffer = new Array2D<LayeredControl.CanvasRectangle>(cx, cy);
 
-            var data = new LayeredControl.CanvasRectangle[cx][];
-
-            var p = new WorkPool();
 
             vv.Layers.Canvas.Hide();
 
@@ -87,60 +127,130 @@ namespace GameOfLife.js
                         {
 
                             ColorDeath = Color.White,
-                            ColorSurvival = Color.Gray,
+                            //ColorSurvival = Color.Gray,
                             ColorBirth = Color.Black
                         };
 
             Func<Color> RandomState = () => Native.Math.random() > 0.5 ? State.ColorDeath : State.ColorBirth;
 
 
-            for (int ti = 0; ti < cx; ti++)
-            {
-                var i = ti;
+            #region reset
 
-                data[i] = new LayeredControl.CanvasRectangle[cy];
-
-                p.Add(
-                    () =>
-                    {
-
-                        for (int j = 0; j < cy; j++)
-                        {
-
-                            var c = new LayeredControl.CanvasRectangle
-                                    {
-                                        BackgroundColor = RandomState(),
-                                        Location = new Rectangle { Left = i * w + 1, Top = j * h + 1, Width = w - 2, Height = h - 2 }
-                                    };
-
-                            vv.DrawRectangleToCanvas(c);
-
-                            data[i][j] = c;
-
-                        }
-
-                        index++;
-                        Console.Log("index: " + index);
-                    });
-            }
-
-            p.Error += Console.LogError;
-
-
-            p.Add(
+            Action Reset =
                 () =>
-                {
-                    Console.Log("done!");
+                buffer.ForEach(
+                    (int x, int y) =>
+                    {
+                        var c = new LayeredControl.CanvasRectangle
+                                {
+                                    BackgroundColor = RandomState(),
+                                    Location = new Rectangle { Left = x * w + 1, Top = y * h + 1, Width = w - 2, Height = h - 2 }
+                                };
 
-                    vv.Layers.Canvas.Show();
+                        vv.DrawRectangleToCanvas(c);
 
-
-
-                    // todo: evolution
-                }
+                        buffer[x, y] = c;
+                    }
                 );
 
-            
+            Reset();
+
+            #endregion
+
+            vv.Layers.Canvas.Show();
+
+            var f = new IHTMLElement(IHTMLElement.HTMLElementEnum.fieldset);
+
+            var chk_enabled = new IHTMLInput(HTMLInputTypeEnum.checkbox);
+            var btn_reset = new IHTMLButton("Randomize");
+
+            btn_reset.onclick += (i) => Reset();
+
+            f.appendChild(btn_reset, chk_enabled, new IHTMLLabel("Activate", chk_enabled));
+
+            f.attachToDocument();
+            f.style.SetLocation(500, 60);
+
+            var NextEvolution = default(Action);
+
+            Action<int> SleepAndEvolve =
+                (timeout) => new Timer((t) => NextEvolution(), timeout, 0);
+
+
+            NextEvolution =
+                () =>
+                {
+
+                    var nextframe = default(Action);
+                    var t = IDate.Now.getTime();
+
+                    if (chk_enabled.@checked)
+                        buffer.ForEach(
+                            (int x, int y) =>
+                            {
+                                Func<int, int, LayeredControl.CanvasRectangle> g =
+                                    (ox, oy) => buffer[x + ox, y + oy];
+
+                                var u = g(0, 0);
+
+                                var c = new[] {
+                                g(-1, -1),
+                                g(-1, 0),
+                                g(-1, 1),
+                                g(0, 1),
+                                g(0, -1),
+                                g(1, -1),
+                                g(1, 0),
+                                g(1, 1)
+                            }.Count(i => i != null && i.BackgroundColor == State.ColorBirth);
+
+
+                                var ncolor = default(Color);
+
+                                var IsDeath = u.BackgroundColor == State.ColorDeath;
+                                var IsBirth = u.BackgroundColor == State.ColorBirth;
+
+                                var Is2 = c == 2;
+                                var Is3 = c == 3;
+                                var Is2or3 = Is2 || Is3;
+
+                                if (IsDeath && Is3)
+                                {
+                                    ncolor = State.ColorBirth;
+                                }
+                                else if (IsBirth && Is2or3)
+                                {
+                                    ncolor = State.ColorBirth;
+                                }
+                                else
+                                {
+                                    ncolor = State.ColorDeath;
+                                }
+
+
+                                nextframe += () =>
+                                                 {
+                                                     u.BackgroundColor = ncolor;
+                                                     u.Update();
+                                                 };
+                            }
+                        );
+
+                    if (nextframe != null)
+                    {
+                        nextframe();
+                        nextframe = null;
+                    }
+
+                    int timeout = (int)(IDate.Now.getTime() - t) - 0;
+
+                    Console.WriteLine("time: " + timeout);
+
+                    SleepAndEvolve(timeout.Max(100));
+                };
+
+            SleepAndEvolve(1);
+
 
         }
 
