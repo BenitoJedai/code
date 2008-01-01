bccacbdf0fd51554ff8e2c26361e7216552b5e6b
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-01-01 14:54:41 +0000

    tutorial step 2

--- javascript/Examples/NatureBoyTestPad/NatureBoyTestPad/js/NatureBoyTestPad.cs
+++ javascript/Examples/NatureBoyTestPad/NatureBoyTestPad/js/NatureBoyTestPad.cs
@@ -4,6 +4,9 @@ using ScriptCoreLib.JavaScript.Extensions;
 using ScriptCoreLib.JavaScript.DOM.HTML;
 using ScriptCoreLib.Shared.Drawing;
 using ScriptCoreLib.Shared.Lambda;
+using ScriptCoreLib.JavaScript.Controls;
+using ScriptCoreLib.JavaScript.Controls.NatureBoy;
+using System;
 
 
 namespace NatureBoyTestPad.js
@@ -13,28 +16,51 @@ namespace NatureBoyTestPad.js
     {
         public NatureBoyTestPad()
         {
+            #region tutorial step 2
+            #region arena
+            var map = new Point(1000, 1000);
 
-            var btn = new IHTMLButton("Hello World!").AttachToDocument();
+            var arena = new ArenaControl();
 
-            var counter = new IHTMLSpan().AttachTo(btn);
+            arena.Layers.Canvas.style.backgroundColor =
+                Color.FromGray(0xc0);
+            arena.SetLocation(
+                Rectangle.Of(0, 0, Native.Window.Width, Native.Window.Height));
 
-            counter.style.margin = "1em";
+            arena.SetCanvasSize(map);
 
-            var i = 0;
+            arena.Control.AttachToDocument();
+            #endregion
 
-            btn.onclick += ev =>
+            #region actor
+            var actor = new Dude2();
+
+            var frames = new DudeAnimationInfo
+            {
+                Frames_Stand = Frames.WolfSoldier,
+                Frames_Walk = Frames.WolfSoldier_Walk
+            };
+
+            actor.Frames = frames.Frames_Stand;
+            actor.AnimationInfo.Frames_Stand = frames.Frames_Stand;
+            actor.AnimationInfo.Frames_Walk = frames.Frames_Walk;
+            actor.Zoom.DynamicZoomFunc = a => 1;
+            actor.Zoom.StaticZoom = 1;
+            actor.SetSize(48, 72);
+            actor.TeleportTo(Native.Window.Width / 2, Native.Window.Height / 2);
+            actor.Direction = Math.PI * 0.5;
+            actor.Control.AttachTo(arena.Layers.Canvas);
+            #endregion
+
+            arena.SelectionClick +=
+                (coords, ev) =>
                 {
-                    i++;
-
-                    counter.innerText = "(" + i + ")";
-
-                    counter.style.color = Color.FromRGB(
-                        0xff.Random(),
-                        0xff.Random(),
-                        0xff.Random()
-                    );
+                    actor.WalkTo(coords);
                 };
+            #endregion
+
         }
+
 
         static NatureBoyTestPad()
         {
