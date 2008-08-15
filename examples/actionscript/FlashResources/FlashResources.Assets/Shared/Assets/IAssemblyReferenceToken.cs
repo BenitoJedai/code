fc0cd3d844389594c54db2e8f4c9d96f9213181f
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-08-15 11:23:16 +0000

    (wpf + flash) images as EmbeddedResource support added

---
+++ examples/actionscript/FlashResources/FlashResources.Assets/Shared/Assets/IAssemblyReferenceToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+using ScriptCoreLib;
+using ScriptCoreLib.Shared;
+
+namespace FlashResources.Shared.Assets
+{
+	/// <summary>
+	/// Without this class some assemblies are not referenced as they only contain
+	/// type mappings but no real type usage.
+	/// </summary>
+	public interface IAssemblyReferenceToken :
+		ScriptCoreLib.Shared.IAssemblyReferenceToken
+	{
+	}
+
+	public interface IAssemblyReferenceTokenNative 
+	{
+	}
+}
---
+++ examples/actionscript/FlashResources/FlashResources.AvalonCode/ActionScript/MyCanvas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using ScriptCoreLib;
+using ScriptCoreLib.ActionScript.Extensions;
+using ScriptCoreLib.Shared.Avalon.Extensions;
+using System.Windows.Media.Effects;
+
+namespace FlashResources.ActionScript
+{
+	[Script]
+	public class MyCanvas : Canvas
+	{
+		// this class is for wpf renderer and flash emulated wpf renderer
+
+		public MyCanvas()
+		{
+			var e = new Image { Source = "assets/FlashResources.Assets/tipsi2.png".ToSource() };
+
+			e.BitmapEffect = new DropShadowBitmapEffect();
+
+			Children.Add(e);
+		}
+
+		
+	}
+}
