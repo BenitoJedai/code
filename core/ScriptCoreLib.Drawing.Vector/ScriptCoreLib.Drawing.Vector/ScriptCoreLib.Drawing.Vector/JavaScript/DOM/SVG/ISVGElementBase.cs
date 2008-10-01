148c2cb18b5bdfaabc1020c86523f58f7f28a596
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-10-01 16:43:11 +0000

    updated for current browsers

--- core/ScriptCoreLib.Drawing.Vector/ScriptCoreLib.Drawing.Vector/ScriptCoreLib.Drawing.Vector/JavaScript/DOM/SVG/ISVGElementBase.cs
+++ core/ScriptCoreLib.Drawing.Vector/ScriptCoreLib.Drawing.Vector/ScriptCoreLib.Drawing.Vector/JavaScript/DOM/SVG/ISVGElementBase.cs
@@ -6,79 +6,101 @@ using ScriptCoreLib.JavaScript.DOM.HTML;
 
 namespace ScriptCoreLib.JavaScript.DOM.SVG
 {
-    [Script(InternalConstructor = true)]
-    public class ISVGElementBase : IHTMLElement
-    {
-        [Script]
-        public class Settings
-        {
-            // http://thomas.tanreisoftware.com/?p=79
+	[Script(InternalConstructor = true)]
+	public class ISVGElementBase : IHTMLElement
+	{
+		[Script]
+		public class Settings
+		{
+			// http://thomas.tanreisoftware.com/?p=79
 
-            public static readonly string[] MimeTypes =
-                new[] { "image/svg+xml", "image/svg-xml", "image/svg" };
+			public static readonly string[] MimeTypes =
+				new[] { "image/svg+xml", "image/svg-xml", "image/svg" };
 
-            public static string MimeType { get { return MimeTypes[0]; } }
+			public static string MimeType { get { return MimeTypes[0]; } }
 
-            static bool _IsSupported;
+			//static bool _IsSupported;
 
-            public static bool IsSupported
-            {
-                get
-                {
-                    // document.implementation.hasFeature("org.w3c.dom.svg", "1.0")
+			public static bool IsSupported
+			{
+				get
+				{
+					// http://code.google.com/android/reference/org/w3c/dom/DOMImplementation.html#hasFeature(java.lang.String,%20java.lang.String)
 
-                    return Native.Document.implementation.hasFeature("org.w3c.dom.svg", "1.0");
-                    /*
-                    var p = Native.Window.navigator.mimeTypes;
+					// document.implementation.hasFeature("org.w3c.dom.svg", "1.0")
+					//http://www.w3.org/TR/SVG11/feature#Structure
+					// fore firefox 2 and earlier
+					if (Native.Document.implementation.hasFeature("org.w3c.dom.svg", "1.0"))
+					{
+						return true;
+					}
 
-                    for (int i = 0; i < p.length; i++)
-                    {
-                        if (MimeTypes.Contains(p[i].type))
-                        {
-                            _IsSupported = true;
+					if (Native.Document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Structure", "1.1"))
+					{
+						return true;
+					}
 
-                            break;
-                        }
+					return false;
 
-                        //Console.WriteLine("mime: " + p[i].type + " : " + p[i].description);
-                    }
-
-                    return _IsSupported;
-                     * */
-                }
-            }
-        }
-
-        public static readonly string NS = "http://www.w3.org/2000/svg";
-
-        public string type;
-        public ISVGElementBase ownerSVGElement;
-        public ISVGElementBase viewportSVGElement;
-
-        protected ISVGElementBase()
-        {
-        }
-
-        protected static ISVGElementBase InternalConstructor()
-        {
-            throw new Exception();
-        }
-
-        public ISVGElementBase(string tag)
-        {
-        }
-
-        internal static ISVGElementBase InternalConstructor(string tag)
-        {
-
-            return (ISVGElementBase)(object) //null;
-                Native.Document.createElementNS(NS, tag);
+					// http://dojotoolkit.org/forum/dojo-0-4-x-legacy/dojo-0-4-x-support/charting-dont-working-under-firefox-3
+					// http://www.nabble.com/SVG-detection-in-Firefox-3-and-Safari-3-td17378932.html
+					// http://www.w3.org/TR/SVG11/feature#Image%27,%271.1%27
 
 
+					//javascript:alert(document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Structure", "1.1"))
+
+					/*
+                    
+				var p = Native.Window.navigator.mimeTypes;
+
+				for (int i = 0; i < p.length; i++)
+				{
+					if (MimeTypes.Contains(p[i].type))
+					{
+						_IsSupported = true;
+
+						break;
+					}
+
+					Console.WriteLine("mime: " + p[i].type + " : " + p[i].description);
+				}
+
+				return _IsSupported;*/
+
+				}
+			}
+		}
+
+		public static readonly string NS = "http://www.w3.org/2000/svg";
+
+		public string type;
+		public ISVGElementBase ownerSVGElement;
+		public ISVGElementBase viewportSVGElement;
+
+		protected ISVGElementBase()
+		{
+		}
+
+		protected static ISVGElementBase InternalConstructor()
+		{
+			throw new Exception();
+		}
+
+		public ISVGElementBase(string tag)
+		{
+		}
+
+		internal static ISVGElementBase InternalConstructor(string tag)
+		{
+
+			return (ISVGElementBase)(object) //null;
+				Native.Document.createElementNS(NS, tag);
 
 
-        }
 
-    }
+
+		}
+
+	}
 
 }
