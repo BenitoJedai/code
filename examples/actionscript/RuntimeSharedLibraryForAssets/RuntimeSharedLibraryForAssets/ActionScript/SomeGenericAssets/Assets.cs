75b17efecf520f57028fd36301d9de9da2b10ddf
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-10-02 07:05:55 +0000

    trying out RSL

---
+++ examples/actionscript/RuntimeSharedLibraryForAssets/RuntimeSharedLibraryForAssets/ActionScript/SomeGenericAssets/Assets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib;
+using ScriptCoreLib.ActionScript;
+
+namespace SomeGenericAssets.ActionScript
+{
+	[Script(IsNative = true)]
+	public class Assets
+	{
+		public const string Path = "assets/SomeGenericAssets";
+
+
+		public Class this[string e]
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
