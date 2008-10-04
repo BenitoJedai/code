c40f7c43739ddee24f42f8913b8821bcf31dc4e8
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-10-04 16:35:02 +0000

    upgraded ScriptResources to fetch constants from its type

--- compiler/ScriptCoreLibA/ScriptAttribute.cs
+++ compiler/ScriptCoreLibA/ScriptAttribute.cs
@@ -208,21 +208,7 @@ namespace ScriptCoreLib
         }
     }
 
-    /// <summary>
-    /// allows a project to have embedded resources, which are to be extracted on the compile time when jsc compiler is finished.
-    /// Current implementation has a special folder called 'web' into which all output generated to. This means that the embedded resources should be within a folder called \web\ in your solution.
-    /// </summary>
-    [global::System.AttributeUsage(AttributeTargets.Assembly, Inherited = false, AllowMultiple = true)]
-    public sealed class ScriptResourcesAttribute : Attribute
-    {
-        public string Value;
 
-        public ScriptResourcesAttribute(string e)
-        {
-            this.Value = e;
-        }
-
-    }
 
 
     /// <summary>
---
+++ compiler/ScriptCoreLibA/Shared/ScriptResourcesAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScriptCoreLib.Shared
+{
+	/// <summary>
+	/// allows a project to have embedded resources, which are to be extracted on the compile time when jsc compiler is finished.
+	/// Current implementation has a special folder called 'web' into which all output generated to. This means that the embedded resources should be within a folder called \web\ in your solution.
+	/// </summary>
+	[global::System.AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+	public sealed class ScriptResourcesAttribute : Attribute
+	{
+		public string Value;
+
+		public ScriptResourcesAttribute(string e)
+		{
+			this.Value = e;
+		}
+
+		public ScriptResourcesAttribute()
+		{
+			// when used on a class we should include
+			// all string constants as paths to resources
+		}
+
+	}
+}
