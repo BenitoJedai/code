c01b0b85034ffce6bfa48c5f912343307d2f3ed2
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-10-05 12:53:42 +0000

    added pump handle

--- examples/actionscript/AvalonPipeMania/AvalonPipeMania.Code/Pipe.PumpToLeft.cs
+++ examples/actionscript/AvalonPipeMania/AvalonPipeMania.Code/Pipe.PumpToLeft.cs
@@ -20,22 +20,46 @@ namespace AvalonPipeMania.Code
 		{
 			public PumpToLeft()
 			{
-				var f = new Factory(KnownAssets.Path.Pipe.PumpToLeft, this.Container);
+				{
+					var f = new Factory(KnownAssets.Path.Pipe.PumpToLeft, this.Container);
 
-				this.Outline = f.ToImage("outline");
+					this.Outline = f.ToImage("outline");
 
-				this.Brown = f.ToImage("brown");
+					this.Brown = f.ToImage("brown");
 
-				this.Green = f.ToImage("green");
-				this.Green.Visibility = Visibility.Hidden;
+					this.Green = f.ToImage("green");
+					this.Green.Visibility = Visibility.Hidden;
 
 
-				this.Water = f.ToWaterImages(
-					"0_8",
-					"8_16"
-				);
+					this.Water = f.ToWaterImages(
+						"0_8",
+						"8_16"
+					);
 
-				this.Glow = f.ToImage("glow");
+					this.Glow = f.ToImage("glow");
+				}
+
+				{
+					var f = new Factory(KnownAssets.Path.Pipe.PumpHandle, this.Container);
+
+					var Handles = f.ToHiddenImages(
+						"1", "2", "3", "4", "5", "6"
+					);
+
+					Action Hide = delegate { };
+
+					(1000 / 23).AtIntervalWithCounter(
+						Counter =>
+						{
+							Hide();
+
+							Handles.AtModulus(Counter).Visibility = Visibility.Visible;
+
+							Hide = () => Handles.AtModulus(Counter).Visibility = Visibility.Hidden;
+						}
+					);
+
+				}
 			}
 		}
 
