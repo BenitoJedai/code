6fa503209fd2a2107f5569e5c2d907fd57c05ec4
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-10-21 10:57:24 +0000

    first draft of the gallery

---
+++ examples/actionscript/AvalonExampleGallery/AvalonExampleGalleryCanvas.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using ScriptCoreLib;
+using ScriptCoreLib.Shared.Avalon.Extensions;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using ScriptCoreLib.Shared.Avalon.TiledImageButton;
+
+namespace AvalonExampleGallery.Shared
+{
+	[Script]
+	public class TiledBackgroundImage : ISupportsContainer
+	{
+		public Canvas Container { get; set; }
+
+		public TiledBackgroundImage(ImageSource src, int width, int height, int x, int y)
+		{
+			this.Container = new Canvas
+			{
+				Width = width * x,
+				Height = height * y
+			};
+
+			for (int i = 0; i < x; i++)
+				for (int j = 0; j < y; j++)
+				{
+					new Image
+					{
+						Source = src,
+						Width = width,
+						Height = height
+					}.MoveTo(i * width, j * height).AttachTo(this.Container);
+				}
+		}
+	}
+
+	[Script]
+	public class AvalonExampleGalleryCanvas : Canvas
+	{
+		public const int DefaultWidth = 480;
+		public const int DefaultHeight = 320;
+
+		public AvalonExampleGalleryCanvas()
+		{
+			Width = DefaultWidth;
+			Height = DefaultHeight;
+
+			Colors.Blue.ToGradient(Colors.Red, DefaultHeight / 4).Select(
+				(c, i) =>
+					new Rectangle
+					{
+						Fill = new SolidColorBrush(c),
+						Width = DefaultWidth,
+						Height = 4,
+					}.MoveTo(0, i * 4).AttachTo(this)
+			).ToArray();
+
+
+			var navbar = new AeroNavigationBar();
+
+			var Container = new Canvas
+			{
+				Width = DefaultWidth,
+				Height = DefaultHeight
+			}.AttachTo(this);
+
+			var bg = new TiledBackgroundImage(
+				"assets/AvalonExampleGallery/bg.png".ToSource(),
+				96,
+				96,
+				5,
+				4
+			).AttachContainerTo(Container);
+
+			new Image
+			{
+				Source =
+					"assets/AvalonExampleGallery/PreviewShadow.png".ToSource(),
+				Width = 196,
+				Height = 90
+			}.MoveTo(0, 9).AttachTo(Container);
+
+			new Image
+			{
+				Source =
+					"assets/AvalonExampleGallery/PreviewSelection.png".ToSource(),
+				Width = 138,
+				Height = 108
+			}.AttachTo(Container);
+
+			new Image
+			{
+				Source = (global::NavigationButtons.Assets.Shared.KnownAssets.Path.Assets + "/Preview.png").ToSource(),
+				Width = 120,
+				Height = 90
+			}.MoveTo(9, 9).AttachTo(Container);
+
+			//bg.Container.Opacity = 0.5;
+
+			var page1 = new NavigationButtons.Code.MyCanvas();
+
+			navbar.History.Add(
+				delegate
+				{
+					page1.Orphanize();
+				},
+				delegate
+				{
+					page1.AttachTo(Container);
+				}
+			);
+
+
+			navbar.Container.AttachTo(this);
+
+
+		}
+	}
+}
---
+++ examples/actionscript/AvalonExampleGallery/AvalonExampleGalleryFlash.cs
@@ -0,0 +1,71 @@
+﻿using ScriptCoreLib;
+using ScriptCoreLib.ActionScript.flash.display;
+using ScriptCoreLib.ActionScript.flash.text;
+using System.Collections.Generic;
+using System;
+using ScriptCoreLib.ActionScript;
+using ScriptCoreLib.ActionScript.Extensions;
+
+
+
+namespace AvalonExampleGallery.ActionScript
+{
+	using TargetCanvas = global::AvalonExampleGallery.Shared.AvalonExampleGalleryCanvas;
+
+	/// <summary>
+	/// Default flash player entrypoint class. See 'tools/build.bat' for adding more entrypoints.
+	/// </summary>
+	[Script, ScriptApplicationEntryPoint(Width = TargetCanvas.DefaultWidth, Height = TargetCanvas.DefaultHeight)]
+	[SWF(width = TargetCanvas.DefaultWidth, height = TargetCanvas.DefaultHeight)]
+	public class AvalonExampleGalleryFlash : Sprite
+	{
+		public AvalonExampleGalleryFlash()
+		{
+			// spawn the wpf control
+			AvalonExtensions.AttachToContainer(new TargetCanvas(), this);
+		}
+
+		static AvalonExampleGalleryFlash()
+		{
+			// add resources to be found by ImageSource
+		
+			// local private assets
+			KnownEmbeddedAssets.RegisterTo(
+				KnownEmbeddedResources.Default.Handlers
+			);
+
+			// register assets from referenced assembly
+			global::NavigationButtons.Assets.ActionScript.KnownAndReferencedEmbeddedAssets.RegisterTo(
+				KnownEmbeddedResources.Default.Handlers
+			);
+		}
+
+
+	}
+
+
+
+	[Script]
+	public class KnownEmbeddedAssets
+	{
+
+
+		[EmbedByFileName]
+		public static Class ByFileName(string e)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void RegisterTo(List<Converter<string, Class>> Handlers)
+		{
+			// assets from current assembly
+			Handlers.Add(e => ByFileName(e));
+
+			//// assets from referenced assemblies
+			//Handlers.Add(e => global::ScriptCoreLib.ActionScript.Avalon.Cursors.EmbeddedAssets.Default[e]);
+			//Handlers.Add(e => global::ScriptCoreLib.ActionScript.Avalon.TiledImageButton.Assets.Default[e]);
+
+		}
+	}
+	
+}
