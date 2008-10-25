4c0d12572fdc886691c50d80961570929762aa55
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-10-25 21:52:26 +0000

    added flash shared object

---
+++ core/ScriptCoreLib/ActionScript/flash/events/SyncEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScriptCoreLib.ActionScript.flash.events
+{
+	// http://livedocs.adobe.com/flex/3/langref/flash/events/SyncEvent.html
+    [Script(IsNative=true)]
+    public class SyncEvent : Event
+    {
+
+		#region Constants
+		/// <summary>
+		/// [static] Defines the value of the type property of a sync event object.
+		/// </summary>
+		public static readonly string SYNC = "sync";
+
+		#endregion
+
+    }
+}
+
