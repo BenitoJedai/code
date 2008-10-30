1ded22bee3b94c38d9a2e2e57d7a2c74fa080d47
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-10-30 13:23:03 +0000

    added basic tween

---
+++ core/ScriptCoreLib.Avalon/ScriptCoreLib.Avalon/Shared/Avalon/Tween/NumericTransmitter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib;
+
+namespace ScriptCoreLib.Shared.Avalon.Tween
+{
+	[Script]
+	public class NumericTransmitter
+	{
+		internal Action<int, int> Output;
+		internal Action<int, int> Input;
+
+		public static implicit operator Action<int, int>(NumericTransmitter e)
+		{
+			return e.Input;
+		}
+	}
+}
