2814f3821709a1300867223e9a2dd405f86f8483
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2008-12-21 16:30:08 +0000

    added wheel support for javascript avalon

---
+++ core/ScriptCoreLib.Avalon/ScriptCoreLib.Avalon/JavaScript/BCLImplementation/System/Windows/Input/MouseWheelEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ScriptCoreLib.JavaScript.BCLImplementation.System.Windows.Input
+{
+	[Script(Implements = typeof(global::System.Windows.Input.MouseWheelEventHandler))]
+	public delegate void __MouseWheelEventHandler(object sender, MouseWheelEventArgs e);
+}
