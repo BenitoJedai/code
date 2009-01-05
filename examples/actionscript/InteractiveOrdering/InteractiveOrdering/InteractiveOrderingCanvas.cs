22508381a7ea094bd94f648a27188dc8e73b0c67
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-01-05 08:45:48 +0000

    step 1 - prototype project and sample data

---
+++ examples/actionscript/InteractiveOrdering/InteractiveOrdering/InteractiveOrderingCanvas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using ScriptCoreLib;
+using ScriptCoreLib.Shared.Avalon.Extensions;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace InteractiveOrdering.Shared
+{
+	[Script]
+	public class InteractiveOrderingCanvas : Canvas
+	{
+		public const int DefaultWidth = 640;
+		public const int DefaultHeight = 480;
+
+		//1.	kuvab võrreldavate piltide kogumid, lubab määrata mitut (näiteks 6)  pilti lineaarselt võrrelda (näiteks kole vs ilusam).
+		//2.	võimaldab muuta saaty arvskaalat (1/9,1/7,1/5,1/3,1,3,5,7,9)
+		//3.	kuvatakse iga erineva pildipaari kohta küsimus nende erinevusest saaty skaalal.
+		//4.	luuakse saaty maatriks tabel, ja arvutatakse nende kaalud
+		//5.	kuvatakse suurima veaga element ning pakutakse võimalust hinnangu muutmiseks
+		//6.	kuvatakse lõplik otsus ja piltide järjestus
+
+		public InteractiveOrderingCanvas()
+		{
+			Width = DefaultWidth;
+			Height = DefaultHeight;
+
+			this.ClipToBounds = true;
+
+			Colors.White.ToGradient(Colors.Blue, DefaultHeight / 4).Select(
+				(c, i) =>
+					new Rectangle
+					{
+						Fill = new SolidColorBrush(c),
+						Width = DefaultWidth,
+						Height = 4,
+					}.MoveTo(0, i * 4).AttachTo(this)
+			).ToArray();
+
+
+
+		}
+	}
+}
---
+++ examples/actionscript/InteractiveOrdering/InteractiveOrdering/InteractiveOrderingDocument.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib;
+using ScriptCoreLib.JavaScript.Extensions;
+using ScriptCoreLib.JavaScript.DOM.HTML;
+
+namespace InteractiveOrdering.JavaScript
+{
+	using TargetCanvas = global::InteractiveOrdering.Shared.InteractiveOrderingCanvas;
+
+	[Script, ScriptApplicationEntryPoint]
+	public class InteractiveOrderingDocument
+	{
+		public InteractiveOrderingDocument(IHTMLElement e)
+		{
+			// wpf here
+			var clip = new IHTMLDiv();
+
+			clip.style.position = ScriptCoreLib.JavaScript.DOM.IStyle.PositionEnum.relative;
+			clip.style.SetSize(TargetCanvas.DefaultWidth, TargetCanvas.DefaultHeight);
+			clip.style.overflow = ScriptCoreLib.JavaScript.DOM.IStyle.OverflowEnum.hidden;
+
+			if (e == null)
+				clip.AttachToDocument();
+			else
+				e.insertPreviousSibling(clip);
+
+			AvalonExtensions.AttachToContainer(new TargetCanvas(), clip);
+
+		}
+
+		static InteractiveOrderingDocument()
+		{
+			typeof(InteractiveOrderingDocument).SpawnTo(i => new InteractiveOrderingDocument(i));
+		}
+
+	}
+}
