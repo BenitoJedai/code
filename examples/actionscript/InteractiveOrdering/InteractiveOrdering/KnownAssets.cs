0391575b73828d221cd9a8651e25fae657c80815
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-01-05 08:45:48 +0000

    step 1 - prototype project and sample data

---
+++ examples/actionscript/InteractiveOrdering/InteractiveOrdering/KnownAssets.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib.Shared;
+using ScriptCoreLib;
+
+namespace InteractiveOrdering
+{
+	namespace Shared
+	{
+
+		[Script]
+		public class KnownAssets : AssetsImplementationDetails
+		{
+			public static readonly KnownAssets Default = new KnownAssets();
+
+			[Script, ScriptResources]
+			public static class Path
+			{
+				// constants defined here define also the assets embedded
+				// to the assembly
+				// example: web/assets/OrcasAvalonApplication/about.txt
+
+				public const string Assets = "assets/InteractiveOrdering";
+
+				public const string DataSet1 = "assets/InteractiveOrdering.DataSet1";
+			}
+
+
+		}
+
+		#region AssetsImplementationDetails
+		public class AssetsImplementationDetails
+		{
+			// This class has the native implementation
+			// JavaScript and ActionScript have their own implementations!
+
+			public string[] FileNames
+			{
+				get
+				{
+					return ScriptCoreLib.CSharp.Extensions.EmbeddedResourcesExtensions.GetEmbeddedResources(null, this.GetType().Assembly);
+				}
+			}
+
+		}
+		#endregion
+
+
+	}
+
+	#region AssetsImplementationDetails
+	namespace JavaScript
+	{
+		[Script(Implements = typeof(Shared.AssetsImplementationDetails))]
+		internal class __AssetsImplementationDetails
+		{
+			public string[] FileNames
+			{
+				[EmbedGetFileNames]
+				get
+				{
+					throw new NotImplementedException();
+				}
+			}
+		}
+	}
+
+	namespace ActionScript
+	{
+		[Script(Implements = typeof(Shared.AssetsImplementationDetails))]
+		internal class __AssetsImplementationDetails
+		{
+			public string[] FileNames
+			{
+				[EmbedGetFileNames]
+				get
+				{
+					throw new NotImplementedException();
+				}
+			}
+
+		}
+
+
+	}
+	#endregion
+}
