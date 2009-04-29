5647e30b523afda55e1fdbe71938eb19480b227d
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-04-29 07:37:32 +0000

    added silverlight test.  the joy...

--- examples/actionscript/FlashPlasma/FlashPlasmaPinkAvalon/Window1.xaml.cs
+++ examples/actionscript/FlashPlasma/FlashPlasmaPinkAvalon/Window1.xaml.cs
@@ -29,16 +29,12 @@ namespace FlashPlasmaPinkAvalon
 			var i = new Image();
 
 			var s = new WriteableBitmap(600, 600, 96, 96, PixelFormats.Pbgra32, null);
-
 			Plasma.generatePlasma(600, 600);
 			var shift = 0;
 
-			var t = new DispatcherTimer();
-
-			t.Tick +=
+			Action Refresh =
 				delegate
 				{
-					shift++;
 
 					var buffer = Plasma.shiftPlasma(shift);
 
@@ -47,15 +43,27 @@ namespace FlashPlasmaPinkAvalon
 
 					for (int j = 0; j < buffer.Length; j++)
 					{
-						Marshal.WriteInt32(s.BackBuffer, j * 4, unchecked((int)buffer[j]));
+						Marshal.WriteInt32(s.BackBuffer, j * 4, unchecked((int)(buffer[j] | 0xff000000) ));
 					}
 
 					s.AddDirtyRect(new Int32Rect(0, 0, 600, 600));
 					s.Unlock();
 				};
 
+			var t = new DispatcherTimer();
+
+			t.Tick +=
+				delegate
+				{
+					shift++;
+					Refresh();
+
+				};
+
 			t.Interval = TimeSpan.FromMilliseconds(10);
 			t.Start();
+
+			Refresh();
 
 			i.Source = s;
 
