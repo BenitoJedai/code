590622e39ac90b6c10c20c9eed2dd34644c27028
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-05-29 16:37:35 +0000

    more stubs

---
+++ examples/actionscript/FlintExplodeImage/FlintExplodeImage/ActionScript/Flint/org/flintparticles/common/activities/Activity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib;
+using FlintExplodeImage.ActionScript.Flint.org.flintparticles.common.emitters;
+
+namespace FlintExplodeImage.ActionScript.Flint.org.flintparticles.common.activities
+{
+	[Script(IsNative = true)]
+	public interface Activity
+	{
+		#region Methods
+		/// <summary>
+		/// The addedToEmitter method is called by the emitter when the Activity is added to it.
+		/// </summary>
+		void addedToEmitter(Emitter emitter);
+
+		/// <summary>
+		/// The getDefaultPriority method is used to order the execution of activities.
+		/// </summary>
+		double getDefaultPriority();
+
+		/// <summary>
+		/// The initialize method is used by the emitter to start the activity.
+		/// </summary>
+		void initialize(Emitter emitter);
+
+		/// <summary>
+		/// The removedFromEmitter method is called by the emitter when the Activity is removed from it.
+		/// </summary>
+		void removedFromEmitter(Emitter emitter);
+
+		/// <summary>
+		/// The update method is used by the emitter to apply the activity.
+		/// </summary>
+		void update(Emitter emitter, double time);
+
+		#endregion
+
+	}
+}
---
+++ examples/actionscript/FlintExplodeImage/FlintExplodeImage/ActionScript/Flint/org/flintparticles/common/events/ParticleEvent.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib;
+using FlintExplodeImage.ActionScript.Flint.org.flintparticles.common.particles;
+
+namespace FlintExplodeImage.ActionScript.Flint.org.flintparticles.common.events
+{
+	[Script(IsNative = true)]
+	public class ParticleEvent
+	{
+		#region Fields
+		/// <summary>
+		/// 
+		/// </summary>
+		public object otherObject;
+
+		/// <summary>
+		/// The particle to which the event relates.
+		/// </summary>
+		public Particle particle;
+
+		/// <summary>
+		/// [static] The event dispatched by an emitter when a pre-existing particle is added to it.
+		/// </summary>
+		public static string PARTICLE_ADDED = "particleAdded";
+
+		/// <summary>
+		/// [static] The event dispatched by an emitter when a particle is created.
+		/// </summary>
+		public static string PARTICLE_CREATED = "particleCreated";
+
+		/// <summary>
+		/// [static] The event dispatched by an emitter when a particle dies.
+		/// </summary>
+		public static string PARTICLE_DEAD = "particleDead";
+
+		/// <summary>
+		/// [static] The event dispatched by an emitter when a particle collides with another object.
+		/// </summary>
+		public static string PARTICLES_COLLISION = "particlesCollision";
+
+		#endregion
+
+		#region Methods
+		#endregion
+
+		#region Constructors
+		/// <summary>
+		/// The constructor creates a ParticleEvent object.
+		/// </summary>
+		public ParticleEvent(string type, Particle particle, bool bubbles, bool cancelable)
+		{
+		}
+
+		/// <summary>
+		/// The constructor creates a ParticleEvent object.
+		/// </summary>
+		public ParticleEvent(string type, Particle particle, bool bubbles)
+		{
+		}
+
+		/// <summary>
+		/// The constructor creates a ParticleEvent object.
+		/// </summary>
+		public ParticleEvent(string type, Particle particle)
+		{
+		}
+
+		/// <summary>
+		/// The constructor creates a ParticleEvent object.
+		/// </summary>
+		public ParticleEvent(string type)
+		{
+		}
+
+		#endregion
+
+
+	}
+}
