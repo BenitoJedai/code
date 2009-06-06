0cd0305de3bc313796f842d98546a83e98756597
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-06-06 10:07:45 +0000

    preparing for appengine template

---
+++ templates/OrcasJavaGoogleApplication/OrcasJavaGoogleApplication/IAssemblyReferenceToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrcasJavaGoogleApplication
+{
+	/// <summary>
+	/// Without this class some assemblies are not referenced as they only contain
+	/// type mappings but no real type usage.
+	/// </summary>
+	public interface IAssemblyReferenceToken :
+		ScriptCoreLibJava.IAssemblyReferenceToken
+	{
+	}
+}
---
+++ templates/OrcasJavaGoogleApplication/OrcasJavaGoogleApplication/tools/build.bat
@@ -0,0 +1,22 @@
+@echo off
+set TargetFileName=%2
+set ConfigurationName=%3
+
+if %ConfigurationName%==Debug goto :eof
+
+@call :jsc
+
+::@call compile.java
+::@call create.jar
+
+goto :eof
+
+:jsc
+pushd ..\bin\%ConfigurationName%
+
+::call c:\util\jsc\bin\jsc.exe %TargetFileName%  -as -js
+call c:\util\jsc\bin\jsc.exe %TargetFileName%  -java
+
+
+popd
+goto :eof
