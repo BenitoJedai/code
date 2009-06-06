4d170553848b22b46f13f9da71cfad4447a41621
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-06-06 10:07:45 +0000

    preparing for appengine template

---
+++ templates/OrcasJavaGoogleApplication/OrcasJavaGoogleApplication/Program.cs
@@ -0,0 +1,54 @@
+using System.Threading;
+using System;
+
+using ScriptCoreLib;
+
+
+namespace OrcasJavaGoogleApplication
+{
+	[Script]
+	public class Program
+	{
+		public static string Text { get; set; }
+
+
+		public static void Main(string[] args)
+		{
+			// Use Release Build to use jsc to generate java program
+			// Use Debug Build to develop on .net
+
+			// doubleclicking on the jar will not show the console
+			unchecked
+			{
+				sbyte a = (sbyte)0x7F;
+				sbyte b = (sbyte)0xFF;
+
+				short ax = (short)((a & 0xff) << 8);
+				short bx = (short)((b & 0xff) << 0);
+
+				Console.WriteLine("ax: " + ax);
+				Console.WriteLine("bx: " + bx);
+			}
+
+			// ax: 32512
+			// bx: 255
+
+			//ax: 32512
+			//bx: 255
+
+			Text = "Hello World";
+
+			var dot = "..";
+
+			for (int i = 0; i < 2; i++)
+			{
+				Thread.Sleep(500);
+
+				Console.Write(dot);
+				//Console.Beep();
+			}
+
+			Console.WriteLine(Text);
+		}
+	}
+}
