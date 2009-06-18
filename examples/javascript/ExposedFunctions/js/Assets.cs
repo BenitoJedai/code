7310b273377119b70c4e46bf94312157eef9f263
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-06-18 20:13:11 +0000

    new example

---
+++ examples/javascript/ExposedFunctions/js/Assets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib;
+using ScriptCoreLib.Shared;
+
+[assembly: ScriptResources(ExposedFunctions.js.Assets.Path)]
+
+namespace ExposedFunctions.js
+{
+	[Script]
+	internal static class Assets
+	{
+		public const string Path = "assets/ExposedFunctions";
+	}
+}
