25f5f035effee2935866fb0c8b464a2d42d507b6
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-06-26 16:05:22 +0000

    mochi api updated

---
+++ core/ScriptCoreLib.Mochi/ScriptCoreLib.Mochi/ActionScript/MochiLibrary/Ad/MochiServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib;
+
+namespace ScriptCoreLib.ActionScript.MochiLibrary.Ad
+{
+    [Script(IsNative = true)]
+    public class MochiServices
+    {
+		/// <summary>
+		/// Retrieves and initializes the MochiServices.swf from MochiAds servers. Any API calls executed prior to the complete download and initialization of the MochiServices.swf are queued and will be executed once the API is available 
+		/// </summary>
+		/// <param name="key"></param>
+		/// <param name="root"></param>
+        public static void connect(string key, object root)
+        {
+
+        }
+    }
+
+
+}
---
+++ core/ScriptCoreLib.Mochi/ScriptCoreLib.Mochi/ActionScript/MochiLibrary/MochiScoresOptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib.ActionScript.MochiLibrary.Ad;
+using ScriptCoreLib.ActionScript.Extensions;
+
+namespace ScriptCoreLib.ActionScript.MochiLibrary
+{
+    [Script]
+    public sealed class MochiScoresOptions
+    {
+        readonly DynamicContainer Data;
+
+		public MochiScoresOptions()
+        {
+            Data = new DynamicContainer { Subject = new object() };
+
+        }
+
+		public string boardID 
+		{
+			set { Data["boardID"] = value; }
+		}
+
+		public int score
+		{
+			set { Data["score"] = value; }
+		}
+
+       
+		public void showLeaderboard()
+        {
+
+			MochiScores.showLeaderboard(Data.Subject);
+        }
+    }
+
+
+}
