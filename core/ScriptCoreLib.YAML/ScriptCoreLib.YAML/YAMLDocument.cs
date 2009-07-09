fe5e80565a2307ccb6aba50c02f8aabc4da717b7
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-07-09 10:37:25 +0000

    YAML!

---
+++ core/ScriptCoreLib.YAML/ScriptCoreLib.YAML/YAMLDocument.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace ScriptCoreLib.YAML
+{
+	// see: http://yaml.org/spec/current.html
+	public class YAMLDocument
+	{
+		const string Indent = " ";
+		const string Assignment = ": ";
+
+		public static object FromScalars(Type t, StringReader r, ContinueAtDelegate c)
+		{
+			var n = Activator.CreateInstance(t);
+			var f = t.GetFields();
+
+			var x = r.ReadLine();
+
+			while (x != null)
+			{
+				if (x.StartsWith(Indent))
+				{
+					var i = x.IndexOf(Assignment);
+
+					var FieldName = x.Substring(Indent.Length, i - Indent.Length);
+					var FieldValue = x.Substring(i + Assignment.Length);
+
+					t.SetFieldValue(FieldName, n, FieldValue);
+					x = r.ReadLine();
+				}
+				else
+				{
+					c(x);
+					x = null;
+				}
+			}
+
+			return n;
+		}
+
+		public static Array FromMappingsSequence(Type t, string data)
+		{
+			var r = new StringReader(data);
+
+			var a = Array.CreateInstance(t, 0);
+
+			var x = default(string);
+
+			Action next = () => x = r.ReadLine();
+			Action revert = next.ApplyAction(k => next = k);
+			next();
+
+			while (x != null)
+			{
+				revert();
+
+				if (x == "-")
+				{
+					var b = Array.CreateInstance(t, a.Length + 1);
+					Array.Copy(a, b, a.Length);
+					((object[])b)[a.Length] = FromScalars(t, r, n => next = () => x = n );
+					a = b;
+				}
+
+				next();
+			}
+
+			return a;
+		}
+
+		public static string WriteMappingsSequence(Type t, params object[] data)
+		{
+			// fields must only be instance and string
+
+			var w = new StringBuilder();
+			var f = t.GetFields();
+
+			foreach (var k in data)
+			{
+				w.AppendLine("-");
+
+				foreach (var i in f)
+				{
+					w.AppendLine(Indent + i.Name + Assignment + i.GetValue(k));
+				}
+			}
+
+			return w.ToString();
+		}
+
+		public delegate void ContinueAtDelegate(string e);
+	}
+
+}
---
+++ core/ScriptCoreLib.YAML/ScriptCoreLib.YAML/YAMLExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScriptCoreLib.YAML
+{
+	internal static class YAMLExtensions
+	{
+		public delegate void ApplyActionDelegate(Action a);
+
+		public static Action ApplyAction(this Action a, ApplyActionDelegate h)
+		{
+			return () => h(a);
+		}
+
+		public static void SetFieldValue(this Type t, string FieldName, object target, string FieldValue)
+		{
+			foreach (var f in t.GetFields())
+			{
+				if (f.Name == FieldName)
+					f.SetValue(target, FieldValue);
+			}
+		}
+	}
+
+
+}
