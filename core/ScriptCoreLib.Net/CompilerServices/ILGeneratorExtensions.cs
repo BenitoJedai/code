120590516218f78db1bc64c8080e555deffad775
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-08-17 06:38:04 +0000

    one step closer to dynamic code generation

---
+++ core/ScriptCoreLib.Net/CompilerServices/ILGeneratorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection.Emit;
+
+namespace ScriptCoreLib.CompilerServices
+{
+	public static class ILGeneratorExtensions
+	{
+		public static void EmitTypeOf(this ILGenerator il, Type t)
+		{
+			il.Emit(OpCodes.Ldtoken, t);
+			il.EmitCall(OpCodes.Call, new Func<RuntimeTypeHandle, Type>(Type.GetTypeFromHandle).Method, null);
+		}
+
+		public static void EmitCall(this ILGenerator il, Action<Type> h, Type t)
+		{
+			il.EmitTypeOf(t);
+			il.EmitCall(OpCodes.Call, h.Method, null);
+
+		}
+	}
+}
---
+++ core/ScriptCoreLib.Net/CompilerServices/PrimitiveServerBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection.Emit;
+
+namespace ScriptCoreLib.CompilerServices
+{
+	public class PrimitiveServerBuilder
+	{
+		// classes in this namespace are to be used to build
+		// alternative versions additionally to user written assemblies
+
+		// types built within this assembly can reference this assembly for previously 
+		// written functionality
+
+		// also not that this assembly depends on ScriptCoreLib but not on ScriptCoreLibJava. 
+		// if we were to build a server with java in mind we should add a reference to ScriptCoreLibJava
+		// to do that we should be referencing ScriptCoreLibJava at some point... but not within this assembly.
+
+		// this assembly shall support flash network providers like nonoba and kongregate
+
+		// the functionality from jsc.server could also be included within this assembly
+		
+		// some functionality shall be refactored to ScriptCoreLib.Net.Server assembly
+		// to enable java support via [Optimization("script")]
+		public static string GetVersionInformation()
+		{
+			return "powered by ScriptCoreLib.Net";
+		}
+
+		public static void StartRouter(Type t)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("will start router: " + t.FullName);
+			Console.ForegroundColor = ConsoleColor.Gray;
+		}
+
+	
+		public static void EmitVersionInformation(ILGenerator il)
+		{
+			// kind of funny as this assembly will have parts where the IL will be compiled to target languages like actionscript
+			// and also this assemlby contains code to build some of the IL which might end up being retranslated to that language
+
+			Func<string> _GetVersionInformation = GetVersionInformation;
+
+			var _a = il.DeclareLocal(typeof(string));
+
+			il.EmitCall(OpCodes.Call, _GetVersionInformation.Method, null);
+			il.Emit(OpCodes.Stloc, _a);
+			il.EmitWriteLine(_a);
+		}
+	}
+}
