eab3552aaf38f82f45c8c4e476b8a8ac106738f8
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2009-11-02 08:51:01 +0000

    web services!

---
+++ compiler/jsc.meta/jsc.meta/Commands/Extend/ExtendToGoogleAppEngineWebService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace jsc.meta.Commands.Extend
+{
+	public class ExtendToGoogleAppEngineWebService
+	{
+		// asp.net web service
+		// running on gae
+		// this implementation could be shared
+		// with php to some extent
+		
+		public FileInfo assembly;
+		public DirectoryInfo staging;
+
+		public FileInfo ant;
+		public DirectoryInfo appengine;
+
+		public string application;
+		public string version;
+
+		public void Invoke()
+		{
+			// find all WebMethods
+			// create a wapper
+			// compile with ant
+
+			// we should rewrite source for jsc
+		}
+	}
+}
