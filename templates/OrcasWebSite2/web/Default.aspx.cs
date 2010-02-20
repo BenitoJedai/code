900fbf14745bc0ba2d0ad948ed1d3ca00c4ccec1
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2010-02-20 11:29:11 +0000

    example works again

--- templates/OrcasWebSite2/web/Default.aspx.cs
+++ templates/OrcasWebSite2/web/Default.aspx.cs
@@ -9,8 +9,11 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using ScriptCoreLib;
 
-public partial class _Default : System.Web.UI.Page 
+[assembly: Script, ScriptTypeFilter(ScriptType.JavaScript)]
+
+public partial class _Default : System.Web.UI.Page
 {
 	protected void Page_Load(object sender, EventArgs e)
 	{
