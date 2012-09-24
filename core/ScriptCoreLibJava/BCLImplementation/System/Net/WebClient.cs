6bef06521c2598c68f9cc35a31e2ed540828d49c
carlogodoy <carlogodoy@d756a6eb-311b-0410-9194-f962947b7fec>
2012-09-24 01:39:02 +0000

    Java POST support for BCLImpl of WebClient.

--- core/ScriptCoreLibJava/BCLImplementation/System/Net/WebClient.cs
+++ core/ScriptCoreLibJava/BCLImplementation/System/Net/WebClient.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using ScriptCoreLib;
 using ScriptCoreLibJava.BCLImplementation.System.ComponentModel;
+using System.Net;
+using java.net;
+using java.io;
 
 namespace ScriptCoreLibJava.BCLImplementation.System.Net
 {
@@ -11,6 +14,8 @@ namespace ScriptCoreLibJava.BCLImplementation.System.Net
 	internal class __WebClient : __Component
 	{
         public Encoding Encoding { get; set; }
+
+        public WebHeaderCollection Headers { get; set; }
 
 		public string DownloadString(string u)
 		{
@@ -44,5 +49,78 @@ namespace ScriptCoreLibJava.BCLImplementation.System.Net
 
 			return w.ToString();
 		}
+        
+        public string UploadString(string u, string method, string data)
+        {
+            return UploadString(new Uri(u), data, method);
+        }
+        
+        public string UploadString(Uri u, string method, string data)
+        {
+            var w = new StringBuilder();
+
+            HttpURLConnection conn = null;
+
+            try
+            {
+                var url = new java.net.URL(u.ToString());
+
+                conn = (HttpURLConnection)url.openConnection();
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+                conn.setInstanceFollowRedirects(false);
+                conn.setRequestMethod(method);
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestProperty("charset", "utf-8");
+                conn.setRequestProperty("content-length", "" + data.Length);
+                conn.setUseCaches(false);
+
+                try
+                {
+
+                    if (Headers != null)
+                    {
+                        foreach (string key in Headers.AllKeys)
+                            conn.addRequestProperty(key, Headers[key]);
+                    }
+                }
+                catch (Exception e)
+                {
+                    //System.Console.WriteLine("ERROR: Failed to write headers. Exception was:" + e.Message);
+                }
+
+                DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+                wr.writeBytes(data);
+                wr.flush();
+                wr.close();
+
+
+
+                //var i = new java.io.InputStreamReader(url.openStream(), "UTF-8");
+                var i = new java.io.InputStreamReader(conn.getInputStream(), "UTF-8");
+                var reader = new java.io.BufferedReader(i);
+
+                // can't we just read to the end?
+                var line = reader.readLine();
+                while (line != null)
+                {
+                    w.AppendLine(line);
+
+                    line = reader.readLine();
+                }
+                reader.close();
+            }
+            catch
+            {
+                // oops
+            }
+
+            if (conn != null)
+                conn.disconnect();
+
+            return w.ToString();
+        }
+
+
 	}
 }
---
+++ core/ScriptCoreLibJava/BCLImplementation/System/Net/WebHeaderCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScriptCoreLib;
+using System.Collections.Specialized;
+
+namespace ScriptCoreLibJava.BCLImplementation.System.Net
+{
+    [Script(Implements = typeof(global::System.Net.WebHeaderCollection))]
+    internal class __WebHeaderCollection : NameValueCollection
+    {
+
+    }
+}
