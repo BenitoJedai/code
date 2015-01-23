7c3112162d73c4941f61fc35c8212658ffba8d9e
zproxy <zproxy@d756a6eb-311b-0410-9194-f962947b7fec>
2015-01-23 22:13:03 +0000

    ok

---
+++ examples/merge/Test/TestHTMLDocumentAnalysis/TestHTMLDocumentAnalysis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHTMLDocumentAnalysis
+{
+    class Program
+    {
+        // we have been here before, have we not
+        // first order of business. commit and then reopen on the red.
+        // this test should explore what do we need to
+        // collect and anlyze a single wep page application 
+        // including a level of indirection.
+        // would we be able to visualize it?
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
